6cb9798989fa89d14e30b6570d352c81640f45d7
KLo94 <kevin-alex-lo@hotmail.ca>
2017-01-27 13:24:44 -0500

    Formatted Login, Sign Up and Landing page.

--- sitestatus/src/components/SignUp.jsx
+++ sitestatus/src/components/SignUp.jsx
@@ -109,67 +109,96 @@ var SignUp = React.createClass({
 
   render: function() {
     console.log(this.state.errorMessages);
+    var errorMessageTextStyle = {
+      color: 'red',
+      fontSize: '8px',
+      background: 'white',
+      width: '100%',
+
+    }
+    var inputItemStyle = {
+      width: '100%'
+    }
+    var divListItemStyle ={
+      width: '100%'
+    }
     return (
     <section>
       <Ons.List>
-        <Ons.ListItem modifier="nodivider">
-          <Ons.Input
-            className="center"
-            value={this.state.company}
-            onChange={this.handleCompanyChange}
-            modifier='underbar'
-            placeholder='Company' />
-          {this.state.errorMessages.company && this.state.errorMessages.company.length > 0 &&
-            <span>{this.state.errorMessages.company}</span>
-          }
+          <Ons.ListItem modifier="nodivider" >
+          <div style={divListItemStyle}>
+            {this.state.errorMessages.company && this.state.errorMessages.company.length > 0 &&
+              <div modifier='nodivider' style={errorMessageTextStyle}>{this.state.errorMessages.company}</div>
+            }
+            <Ons.Input 
+              style={inputItemStyle}
+              className="center"
+              value={this.state.company}
+              onChange={this.handleCompanyChange}
+              modifier='underbar'
+              placeholder='Company Name' />
+          </div>
         </Ons.ListItem>
         <Ons.ListItem modifier="nodivider">
-          <Ons.Input
-            className="center"
-            value={this.state.firstName}
-            onChange={this.handleFirstNameChange}
-            modifier='underbar'
-            placeholder='first' />
-          {this.state.errorMessages.firstName && this.state.errorMessages.firstName.length > 0 &&
-            <span>{this.state.errorMessages.firstName}</span>
-          }
+          <div style={divListItemStyle}>
+            {this.state.errorMessages.firstName && this.state.errorMessages.firstName.length > 0 &&
+            <div modifier='nodivider' style={errorMessageTextStyle}>{this.state.errorMessages.firstName}</div>
+            }
+            <Ons.Input
+              style={inputItemStyle}
+              className="center"
+              value={this.state.firstName}
+              onChange={this.handleFirstNameChange}
+              modifier='underbar'
+              placeholder='First Name' />
+          </div>
         </Ons.ListItem>
         <Ons.ListItem modifier="nodivider">
-          <Ons.Input
-            className="center"
-            value={this.state.lastName}
-            onChange={this.handleLastNameChange}
-            modifier='underbar'
-            placeholder='last' />
-          {this.state.errorMessages.lastName && this.state.errorMessages.lastName.length > 0 &&
-            <span>{this.state.errorMessages.lastName}</span>
-          }
+          <div style={divListItemStyle}>
+            {this.state.errorMessages.lastName && this.state.errorMessages.lastName.length > 0 &&
+            <div modifier='nodivider' style={errorMessageTextStyle}>{this.state.errorMessages.lastName}</div>
+            }
+            <Ons.Input
+              style={inputItemStyle}
+              className="center"
+              value={this.state.lastName}
+              onChange={this.handleLastNameChange}
+              modifier='underbar'
+              placeholder='Last Name' />
+          </div>
+        </Ons.ListItem>
+        
+        <Ons.ListItem modifier="nodivider">
+          <div style={divListItemStyle}>
+            {this.state.errorMessages.emailAddress && this.state.errorMessages.emailAddress.length > 0 &&
+            <div modifier='nodivider' style={errorMessageTextStyle}>{this.state.errorMessages.emailAddress}</div>
+            }
+            <Ons.Input
+              style={inputItemStyle}
+              className="center"
+              value={this.state.emailAddress}
+              onChange={this.handleEmailAddressChange}
+              modifier='underbar'
+              placeholder='Email' />
+          </div>
         </Ons.ListItem>
         <Ons.ListItem modifier="nodivider">
-          <Ons.Input
-            className="center"
-            value={this.state.emailAddress}
-            onChange={this.handleEmailAddressChange}
-            modifier='underbar'
-            placeholder='email' />
-          {this.state.errorMessages.emailAddress && this.state.errorMessages.emailAddress.length > 0 &&
-            <span>{this.state.errorMessages.emailAddress}</span>
-          }
-        </Ons.ListItem>
-        <Ons.ListItem modifier="nodivider">
-          <Ons.Input
-            style={{width: '100%', 'paddingRight': '14px'}}
-            className="center"
-            value={this.state.password}
-            onChange={this.handlePasswordChange}
-            modifier='underbar'
-            placeholder='Password' />
-          {this.state.errorMessages.password && this.state.errorMessages.password.length > 0 &&
-            <span>{this.state.errorMessages.password}</span>
-          }
+          <div style={divListItemStyle}>
+            {this.state.errorMessages.password && this.state.errorMessages.password.length > 0 &&
+            <div modifier='nodivider' style={errorMessageTextStyle}>{this.state.errorMessages.password}</div>
+            }
+            <Ons.Input
+              style={inputItemStyle}
+              type= 'Password'
+              className="center"
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+              modifier='underbar'
+              placeholder='Password' />
+          </div>
         </Ons.ListItem>
         <Ons.ListItem>
-          <Ons.Button onClick={this.props.submit}>Sign Up</Ons.Button>
+          <Ons.Button modifier='large' onClick={this.props.submit}>Sign Up</Ons.Button>
         </Ons.ListItem>
       </Ons.List>
     </section>
