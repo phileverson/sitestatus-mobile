5ed7ebbe685193248bcd1f1d00ef04e1ce1dc5a1
KLo94 <kevin-alex-lo@hotmail.ca>
2017-02-05 15:11:43 -0500

    adding error validation and text box for notes

--- sitestatus/src/components/NewContractor.jsx
+++ sitestatus/src/components/NewContractor.jsx
@@ -155,70 +155,126 @@ var NewContractor = React.createClass({
 	},
 
 	render: function() {
+		console.log(this.state.errorMessages);
+	    var errorMessageTextStyle = {
+	      color: 'red',
+	      fontSize: '8px',
+	      background: 'white',
+	      width: '100%',
+
+	    }
+	    var inputItemStyle = {
+	      width: '100%'
+	    }
+	    var divListItemStyle ={
+	      width: '100%'
+	    }
+	    var noteBoxInside ={
+	      width: '98%',
+	      height: '100%',
+	      borderStyle: 'solid',
+	      borderWidth: '1px',
+	      borderColor: '#ccc',
+	      fontSize: '15px',
+    	  lineHeight: '1',
+    	  fontFamily: '-apple-system, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif'
+	    }
+	    var noteBoxOutside ={
+	      width: '100%',
+	      height: '100px',
+	      
+	      
+	    }
 		return (
 			<Ons.Page renderToolbar={this.renderToolbar}>
 				<section>
 					<Ons.List>
 						<Ons.ListItem modifier="nodivider">
-							<Ons.Input
-							className="center"
-							value={this.state.firstName}
-							onChange={this.handleFirstNameChange}
-							modifier='underbar'
-							placeholder='first' />
+							<div style={divListItemStyle}>
 							{this.state.errorMessages.firstName && this.state.errorMessages.firstName.length > 0 &&
-							<span>{this.state.errorMessages.firstName}</span>
-							}
+				              <div modifier='nodivider' style={errorMessageTextStyle}>{this.state.errorMessages.firstName}</div>
+				            }
+								<Ons.Input
+								style={inputItemStyle}
+								className="center"
+								value={this.state.firstName}
+								onChange={this.handleFirstNameChange}
+								modifier='underbar'
+								placeholder='First Name' />
+								{this.state.errorMessages.firstName && this.state.errorMessages.firstName.length > 0 &&
+								<span>{this.state.errorMessages.firstName}</span>
+								}
+							</div>
 						</Ons.ListItem>
 						<Ons.ListItem modifier="nodivider">
-							<Ons.Input
-							className="center"
-							value={this.state.lastName}
-							onChange={this.handleLastNameChange}
-							modifier='underbar'
-							placeholder='last name' />
+							<div style={divListItemStyle}>
+							{this.state.errorMessages.lastName && this.state.errorMessages.lastName.length > 0 &&
+				              <div modifier='nodivider' style={errorMessageTextStyle}>{this.state.errorMessages.lastName}</div>
+				            }
+								<Ons.Input
+								style={inputItemStyle}
+								className="center"
+								value={this.state.lastName}
+								onChange={this.handleLastNameChange}
+								modifier='underbar'
+								placeholder='Last Name' />
+							</div>
 						</Ons.ListItem>
 						<Ons.ListItem modifier="nodivider">
-							<Ons.Input
-							className="center"
-							value={this.state.phone}
-							onChange={this.handlePhoneChange}
-							modifier='underbar'
-							placeholder='phone' />
+							<div style={divListItemStyle}>
+							{this.state.errorMessages.phone && this.state.errorMessages.phone.length > 0 &&
+				              <div modifier='nodivider' style={errorMessageTextStyle}>{this.state.errorMessages.phone}</div>
+				            }
+								<Ons.Input
+								style={inputItemStyle}
+								className="center"
+								value={this.state.phone}
+								onChange={this.handlePhoneChange}
+								modifier='underbar'
+								placeholder='Phone' />
+							</div>
 						</Ons.ListItem>
 						<Ons.ListItem modifier="nodivider">
-							<Ons.Input
-							className="center"
-							value={this.state.emailAddress}
-							onChange={this.handleEmailAddressChange}
-							modifier='underbar'
-							placeholder='email' />
+							<div style={divListItemStyle}>
+								<Ons.Input
+								style={inputItemStyle}
+								className="center"
+								value={this.state.emailAddress}
+								onChange={this.handleEmailAddressChange}
+								modifier='underbar'
+								placeholder='Email' />
+							</div>
 						</Ons.ListItem>
 						<Ons.ListItem modifier="nodivider">
-							<Ons.Input
-							className="center"
-							value={this.state.company}
-							onChange={this.handleCompanyChange}
-							modifier='underbar'
-							placeholder='comp bro' />
+							<div style={divListItemStyle}>
+								<Ons.Input
+								style={inputItemStyle}
+								className="center"
+								value={this.state.company}
+								onChange={this.handleCompanyChange}
+								modifier='underbar'
+								placeholder='Company' />
+							</div>
 						</Ons.ListItem>
 						<Ons.ListItem modifier="nodivider">
-							<Ons.Input
-							style={{width: '100%', 'paddingRight': '14px'}}
-							className="center"
-							value={this.state.trade}
-							onChange={this.handleTradeChange}
-							modifier='underbar'
-							placeholder='trade' />
+							<div style={divListItemStyle}>
+								<Ons.Input
+								style={inputItemStyle}
+								value={this.state.trade}
+								onChange={this.handleTradeChange}
+								modifier='underbar'
+								placeholder='Trade' />
+							</div>
 						</Ons.ListItem>
 						<Ons.ListItem modifier="nodivider">
-							<Ons.Input
-							style={{width: '100%', 'paddingRight': '14px'}}
-							className="center"
-							value={this.state.note}
-							onChange={this.handleNoteChange}
-							modifier='underbar'
-							placeholder='note' />
+							<div style= {noteBoxOutside}>
+								<textarea
+								style={noteBoxInside}
+								value={this.state.note}
+								onChange={this.handleNoteChange}
+								modifier='underbar'
+								placeholder='Note' ></textarea>
+							</div>
 						</Ons.ListItem>
 					</Ons.List>
 				</section>
