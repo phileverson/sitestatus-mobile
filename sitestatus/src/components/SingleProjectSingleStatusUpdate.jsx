c7a6579138501886f91ee26349a37b2058d7a729
KLo94 <kevin-alex-lo@hotmail.ca>
2017-02-13 15:25:29 -0500

    Added SMS and Telephone Icons to Update Details Page. Reformatted

--- sitestatus/src/components/SingleProjectSingleStatusUpdate.jsx
+++ sitestatus/src/components/SingleProjectSingleStatusUpdate.jsx
@@ -24,6 +24,11 @@ var SingleProjectSingleStatusUpdate = React.createClass({
 		var phoneNum = this.state.relatedContractor.phone;
 		window.location.href='tel://'+phoneNum;
 	},
+	handleText: function(e){
+		console.log(this.state.relatedContractor.phone);
+		var phoneNum = this.state.relatedContractor.phone;
+		window.location.href='sms://'+phoneNum;
+	},
 
 	render: function() {
 		// console.log(this.state.singleUpdate);
@@ -42,22 +47,43 @@ var SingleProjectSingleStatusUpdate = React.createClass({
 	      borderColor: '#ccc',
 	      borderWidth: '1pt'
 	    }
+	    var headerStyle={
+	    	paddingTop: '4pt'
+	    }
+	    var buttonStyle={
+	    	marginLeft: '6px'
+		}
 		return (
 		
 			<section>
 				<Ons.List>
+					<Ons.ListHeader style={headerStyle}>
+						<b>Contractor:</b>
+					</Ons.ListHeader>
 					<Ons.ListItem>
-						<b>{this.state.relatedContractor.firstName+" "+this.state.relatedContractor.lastName+" | "+this.state.relatedContractor.trade}</b>
+					<div className= 'left'>
+						{this.state.relatedContractor.firstName+" "+this.state.relatedContractor.lastName+" | "+this.state.relatedContractor.trade}
+					</div>
+					<div className = 'right'>
+						<Ons.Button style={buttonStyle} value={this.state.relatedContractor.phone} onClick={this.handleCall}>
+							<Ons.Icon icon='fa-phone'/>
+						</Ons.Button>
+						<Ons.Button style={buttonStyle} value={this.state.relatedContractor.phone} onClick={this.handleText}>
+							<Ons.Icon icon='comment-o'/>
+						</Ons.Button>
+					</div>
 					</Ons.ListItem>
+					<Ons.ListHeader style={headerStyle}>
+						<b>Date Sent:</b>
+					</Ons.ListHeader>
 					<Ons.ListItem>
-						{"Date Sent: "+this.state.singleUpdate['Date Sent']}
+						{this.state.singleUpdate['Date Sent']}
 					</Ons.ListItem>
+					<Ons.ListHeader style={headerStyle}>
+						<b>Update:</b>
+					</Ons.ListHeader>
 					<Ons.ListItem>
 						{this.state.singleUpdate.Body}
-					</Ons.ListItem>
-					<Ons.ListItem>
-						<Ons.Button value={this.state.relatedContractor.phone} onClick={this.handleCall}>Call {this.state.relatedContractor.firstName}</Ons.Button>
-						<a href="tel://1-555-555-5555">+1 (555) 555-5555</a>
 					</Ons.ListItem>
 				</Ons.List>
 			</section>
